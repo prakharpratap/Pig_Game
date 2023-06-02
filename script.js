e5040324503b64c5b359851b479736d7eb830d48
Prakhar Singh <prakhar0401@gmail.com>
2023-06-02 16:22:40 +0530

    added state

--- script.js
+++ script.js
@@ -12,14 +12,23 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 //Startin conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+function init() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+}
+init();
 function switchPlayer() {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
@@ -82,4 +91,9 @@ btnHold.addEventListener("click", () => {
         .classList.remove("player--active");
     }
   }
+});
+
+//new game functionality
+btnNew.addEventListener("click", () => {
+  init();
 });
